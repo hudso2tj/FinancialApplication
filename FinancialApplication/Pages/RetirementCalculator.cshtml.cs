1af4dcdcb1f4bda255a1fb68f78767e67b1254fc
hudso2tj <hudso2tj@dukes.jmu.edu>
2024-12-31 21:50:28 -0500

    Updated with 5/6 fully functional tools.

--- FinancialApplication/Pages/RetirementCalculator.cshtml.cs
+++ FinancialApplication/Pages/RetirementCalculator.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinancialApplication.Pages
 {
     public class RetirementCalculatorModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        [Required]
+        [Range(16, 99, ErrorMessage = "Enter age 16-99")]
+        public int age { get; set; }
+
+        [BindProperty]
+        public int retirement_age { get; set; }
+        public void OnPost()
         {
+
         }
     }
 }
