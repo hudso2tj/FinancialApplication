7992c6763a2f86412327470b5e928ebfd6cec230
hudso2tj <hudsontj20@gmail.com>
2025-01-02 17:32:58 -0500

    Basic functionality for retirement calculator complete

--- FinancialApplication/Pages/RetirementCalculator.cshtml.cs
+++ FinancialApplication/Pages/RetirementCalculator.cshtml.cs
@@ -12,10 +12,29 @@ namespace FinancialApplication.Pages
         public int age { get; set; }
 
         [BindProperty]
+        [Required]
+        [Range(62, 70, ErrorMessage = "Enter age 62-70")]
         public int retirement_age { get; set; }
+
+        [BindProperty]
+        [Range(0, int.MaxValue)]
+        public int current_investments { get; set; }
+
+        [BindProperty]
+        [Range(0, int.MaxValue)]
+        public int contributions { get; set; }
+
+        [BindProperty]
+        public double annual_returns { get; set; }
+
+        public int time { get; set; }
+        public double future_value { get; set; }
         public void OnPost()
         {
-
+            time = (retirement_age - age) * 12;
+            annual_returns = (annual_returns / 100) / 12;
+            future_value = contributions * ((Math.Pow(1 + annual_returns, time) - 1) / annual_returns) +
+        current_investments * Math.Pow(1 + annual_returns, time);
         }
     }
 }
